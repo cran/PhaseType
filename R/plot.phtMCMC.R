f76310f940c2cd43f176365e73dc840eb01d8db3
Louis Aslett <louis@maths.tcd.ie>
2012-09-07 00:00:00 +0000

    version 0.1.2

--- R/plot.phtMCMC.R
+++ R/plot.phtMCMC.R
@@ -6,11 +6,11 @@ plot.phtMCMC <- function(x, ...) {
 	print(
 		ggplot(melt(as.data.frame(x$samples[seq(StartEndThin[1], StartEndThin[2], StartEndThin[3]),]), id.vars=NULL)) +
 		geom_line(aes_string(x="1:length(value)", y="value")) +
-		geom_smooth(aes_string(x="1:length(value)", y="value")) +
+		geom_smooth(aes_string(x="1:length(value)", y="value"), method="glm") +
 		#geom_hline(aes(yintercept=value), data=truth, colour="red", linetype="dashed") +
 		facet_wrap(~variable, scales="free") +
 		#theme_grey(base_family="serif", base_size=11) +
-		opts(title = "Parameter Traces") + #, plot.title = theme_text(size=14, face="bold", family="serif")) +
+		ggtitle("Parameter Traces") + #, plot.title = theme_text(size=14, face="bold", family="serif")) +
 		xlab("Iteration") + ylab("Parameter Value")
 	)
 	
@@ -21,7 +21,7 @@ plot.phtMCMC <- function(x, ...) {
 		#geom_vline(aes(xintercept=value), data=truth, colour="red") +
 		facet_wrap(~variable, scales="free") +
 		#theme_grey(base_family="serif", base_size=11) +
-		opts(title = "Marginal Posterior Densities") + #, plot.title = theme_text(size=14, face="bold", family="serif")) +
+		ggtitle("Marginal Posterior Densities") + #, plot.title = theme_text(size=14, face="bold", family="serif")) +
 		xlab("Parameter Value") + ylab("Density")
 	)
 }
